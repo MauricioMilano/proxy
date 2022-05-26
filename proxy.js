f9616c7ca1758cb8304ac6ddc78ea7acd97e7498
Mauricio Milano <mauricio.milano@ibm.com>
2022-05-26 02:01:43 -0300

    code moralized

--- proxy.js
+++ proxy.js
@@ -2,42 +2,43 @@ const { response } = require("express");
 const querystring = require("querystring");
 const express = require("express");
 const axios = require("axios").default;
-const https = require("https")
-axios.defaults.headers.post['Content-Type'] = "application/json"
+const https = require("https");
+axios.defaults.headers.post["Content-Type"] = "application/json";
 const app = express();
-const port = process.env.PORT || 80
-axios.defaults.httpsAgent = new https.Agent({  
-    rejectUnauthorized: false
-})
+const port = process.env.PORT || 80;
+axios.defaults.httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
+let destinyUrl = process.env.DESTINY_URL.slice(-1) == "/" ? process.env.DESTINY_URL : process.env.DESTINY_URL + "/";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
+  
+
+
   try {
-    let response = await axios.get(
-      process.env.DESTINY_URL + "?" + querystring.stringify(req.query),
-      {
-        headers: req.headers,
-      }
-    );
+    console.log(`Sending request to : ${destinyUrl}`);
+    let url = destinyUrl + "?" + querystring.stringify(req.query);
+    let response = await axios.get(url);
     res.send(response.data).status(200);
   } catch (err) {
-    console.log(`deu ruim: ${err}`);
+    console.log(`Error: ${err}`);
     res.sendStatus(err.response.status);
   }
 });
 app.post("/", async (req, res) => {
   try {
-    console.log("enviando uma requisição");
-    let response = await axios.post(process.env.DESTINY_URL, req.body);
-    console.log("recebeu uma resposta");
+    console.log(`Sending request to : ${destinyUrl} \nBody:\n${JSON.stringify(req.body, null, 2)}`);
+    let response = await axios.post(destinyUrl, req.body);
+    console.log(`Response received from request. Redirecting response`);
     res.send(response.data).status(200);
   } catch (err) {
-    console.log(`deu ruim: ${err}`);
+    console.log(`Exception was caught: ${err}`);
     res.sendStatus(err.response.status);
   }
 });
 app.listen(port, () => {
-  console.log(`[${process.pid}] Example app listening on port ${port}`);
+  console.log(`[${process.pid}] starting proxy on port ${port}`);
 });
